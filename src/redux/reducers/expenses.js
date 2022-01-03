9462e98d8cde724dd335a597f68536f88a786f9c
rohit rai <rohitrai.dev@gmail.com>
2022-01-03 19:25:32 +0530

    redux store setup do9ne

---
+++ src/redux/reducers/expenses.js
@@ -0,0 +1,10 @@
+const initialState={
+    expenseList:[]   // if u use = it will throw "SHORT HAND INITIALIZATION ERROR"
+
+}
+export const expenseReducer=(state=initialState,action)=>{
+switch(action.type){
+    default:
+        return state
+}
+}
---
+++ src/redux/store/index.js
@@ -0,0 +1,8 @@
+import { combineReducers, createStore } from "redux";
+import { expenseReducer } from "../reducers/expenses";
+const initialState={}
+const reducer=combineReducers({
+    expenses:expenseReducer
+})
+const store=createStore(reducer,initialState)
+export default store
